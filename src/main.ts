a23eff8de13a7f8c7e4febe371272681448aae72
Kutay Araz <kutayaraz@gmail.com>
2024-07-18 00:03:11 +0300

    feat: add helmet package

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from 'cookie-parser';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        objectSrc: ["'none'"],
+        upgradeInsecureRequests: [],
+      },
+    },
+    frameguard: false,  // Disables the `X-Frame-Options` header.
+  }));
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
