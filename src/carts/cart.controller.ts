8cad14c43d9820c3d42715313709851e4e26ce91
Kutay Araz <kutayaraz@gmail.com>
2024-08-24 23:01:41 +0300

    feat: add swagger for api documentation
    Add ApiOperation decorator to cart controller

--- src/carts/cart.controller.ts
+++ src/carts/cart.controller.ts
@@ -15,6 +15,7 @@ import { SkipThrottle } from "@nestjs/throttler";
 import { CartOperationsService } from "./services/cart-operations.service";
 import { CartService } from "./services/carts.service";
 import { LocalCartService } from "./services/local-cart.service";
+import { ApiOperation } from "@nestjs/swagger";
 
 @Controller("carts")
 export class CartController {
@@ -27,18 +28,21 @@ export class CartController {
   @Public()
   @SkipThrottle()
   @Post("local")
+  @ApiOperation({ summary: 'Get local cart information' })
   async getLocalCartInformation(@Body() localCartDto: CartItemDto[]) {
     return await this.localCartService.getLocalCartInformation(localCartDto);
   }
 
   @UseGuards(AtGuard)
   @Get("user")
+  @ApiOperation({ summary: 'Get user cart' })
   async getUserCart(@GetCurrentUserId() id: number) {
     return await this.cartsService.getUserCart(id);
   }
 
   @UseGuards(AtGuard)
   @Post("buy-now")
+  @ApiOperation({ summary: 'Get buy now cart info' })
   async getBuyNowCartInfo(@Body() cartItem: CartItemDto) {
     return await this.localCartService.getBuyNowCartInfo(
       cartItem.productId,
@@ -48,6 +52,7 @@ export class CartController {
 
   @UseGuards(AtGuard)
   @Patch("merge-carts")
+  @ApiOperation({ summary: 'Merge local cart with user cart' })
   async mergeCarts(
     @GetCurrentUserId() userId: number,
     @Body() cartItems: CartItemDto[],
@@ -58,6 +63,7 @@ export class CartController {
   @UseGuards(AtGuard)
   @SkipThrottle()
   @Patch("user/item")
+  @ApiOperation({ summary: 'Update cart item quantity' })
   async updateItemQuantity(
     @GetCurrentUserId() userId: number,
     @Body() cartItem: CartItemDto,
@@ -72,6 +78,7 @@ export class CartController {
   @UseGuards(AtGuard)
   @SkipThrottle()
   @Post("user/item")
+  @ApiOperation({ summary: 'Add item to cart' })
   async addProductToCart(
     @GetCurrentUserId() userId: number,
     @Body() cartItem: CartItemDto,
@@ -86,6 +93,7 @@ export class CartController {
   @UseGuards(AtGuard)
   @SkipThrottle()
   @Delete("user/item/:productId")
+  @ApiOperation({ summary: 'Remove item from cart' })
   async removeCartItem(
     @GetCurrentUserId() userId: number,
     @Param("productId") productId: number,
@@ -95,6 +103,7 @@ export class CartController {
 
   @UseGuards(AtGuard)
   @Delete("clear-cart")
+  @ApiOperation({ summary: 'Clear user cart' })
   async clearCart(@GetCurrentUserId() userId: number) {
     return await this.cartsService.clearCart(userId);
   }
